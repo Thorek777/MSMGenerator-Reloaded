0735b30c2d58242f10dc6e970abf39e945a8eaa8
Thorek777 <75499293+Thorek777@users.noreply.github.com>
2021-03-12 18:49:01 +0100

    Kod dłuższy, ale czytelny.
    No.

--- MSMGenerator-Reloaded/MSMGenerator-Reloaded.h
+++ MSMGenerator-Reloaded/MSMGenerator-Reloaded.h
@@ -7,8 +7,29 @@
 using namespace std;
 using namespace filesystem;
 
-string sciezka1 = "Models/", sciezka2 = "SourceSkins/", sciezka3 = "TargetSkins/", sciezka4 = "Faces/", sciezka5;
-vector<string> pliki1 = {}, pliki2 = {}, pliki3 = {}, pliki4 = {};
-int wybor, ile_wpisow_stworzyc, aktualna_liczba_wpisow, aktualna_liczba_index, model_twarzy, i, licznik_plikow1, licznik_plikow2, licznik_plikow3, licznik_plikow4;
+string sciezka1 = "Models/";
+string sciezka2 = "SourceSkins/";
+string sciezka3 = "TargetSkins/";
+string sciezka4 = "Faces/";
+string sciezka5;
+
+vector<string> pliki1 = {};
+vector<string> pliki2 = {};
+vector<string> pliki3 = {};
+vector<string> pliki4 = {};
+
+int wybor;
+int ile_wpisow_stworzyc;
+int aktualna_liczba_wpisow;
+int aktualna_liczba_index;
+int model_twarzy;
+int i;
+int licznik_plikow1;
+int licznik_plikow2;
+int licznik_plikow3;
+int licznik_plikow4;
+
 bool rozpoczynanie_od_zera = false;
-void sprawdzanie(), generacja();
+
+void sprawdzanie();
+void generacja();
